5ae9ffdfe885dc578ef667433827d72da4e4f4c7
Grant Michael Schonhoff <schonho2@msu.edu>
2019-07-01 00:15:44 -0400

    set up makeAccount scene 2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -36,35 +36,47 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-
-        primaryStage.setTitle("Personal Password Manager");
-        primaryStage.initStyle(StageStyle.DECORATED);
+        File f = new File("src\\sample\\stylesheet.css");
         Image i = new Image(getClass().getResourceAsStream("images/lock.png"));
-        primaryStage.getIcons().add(i);
 
         // Create the registration form grid pane
         GridPane gridPane = createRegistrationFormPane();
         // Add UI controls to the registration form grid pane
         addUIControls(gridPane);
         // Create a scene with registration form grid pane as the root node
-        scene1 = new Scene(gridPane, 800, 500);
+
+        Button button1= new Button("Go to scene 2");
+        button1.setOnAction(e -> primaryStage.setScene(scene2));
+
+        VBox layout1= new VBox(20);
+        layout1.getChildren().addAll(gridPane, button1);
+        scene1 = new Scene(layout1, 800, 500);
         // Gets css file used for so styling
-        File f = new File("src\\sample\\stylesheet.css");
+
         scene1.getStylesheets().clear();
         scene1.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
+
+        GridPane gridPane2 = createRegistrationFormPane();
+        // Add UI controls to the registration form grid pane
+        addUIControls(gridPane2);
 
         //Scene 2
         Label label2= new Label("This is the second scene");
         Button button2= new Button("Go to scene 1");
         button2.setOnAction(e -> primaryStage.setScene(scene1));
         VBox layout2= new VBox(20);
-        layout2.getChildren().addAll(label2, button2);
-        scene2= new Scene(layout2,300,250);
+        layout2.getChildren().addAll(gridPane2, button2);
+        scene2= new Scene(layout2,800,510);
+
+        scene2.getStylesheets().clear();
+        scene2.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
 
         // Set the scene in primary stage
-        primaryStage.setScene(scene1);
-
-        primaryStage.show();
+        window.setScene(scene1);
+        window.setTitle("Personal Password Manager");
+        window.initStyle(StageStyle.DECORATED);
+        window.getIcons().add(i);
+        window.show();
     }
 
 
